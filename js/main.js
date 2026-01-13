646df669d4b6b47c0501699e80f447b28f192bbc
Renan Chagas <111081634+MrChagas@users.noreply.github.com>
2026-01-13 20:24:11 -0300

    Atualização 13-01
    Adição de seções
    Botão Scroll
    Organização de Imagens

--- js/main.js
+++ js/main.js
@@ -51,9 +51,35 @@ const swiperServices = new Swiper('.services__swiper', {
 });
 
 /*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up')
+//when the scroll is higher than 350vw height, add the show-scroll class to the a tag with the scroll
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                                            : scrollUp.classList.remove('show-scroll')
+}
 
-
+window.addEventListener('scroll', scrollUp)
+scrollUp()
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
 
+// Link the ID of each section (section id="home") to each link (a href="#home") 
+// and activate the link with the class .active-link
+const scrollActive = () => {
+   // We get the position by scrolling down
+   const scrollY = window.scrollY
+
+   sections.forEach(section => {
+      const id = section.id, // id of each section
+            top = section.offsetTop - 50, // Distance from the top edge
+            height = section.offsetHeight, // Element height
+            link = document.querySelector('.nav__menu a[href*=' + id + ']') // id nav link
+
+      if(!link) return
+
+      link.classList.toggle('active-link', scrollY > top && scrollY <= top + height)
+   })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
